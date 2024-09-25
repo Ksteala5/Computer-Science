374990f22aae0e36f89e8e80f375a9b732ef8116
Ksteala5 <bartunekkaden@gmail.com>
2024-09-25 14:41:23 -0500

    FinAL ANimal Survey

---
+++ Unit 2 - Functions and Variables/practice_sept-24.py
@@ -0,0 +1,9 @@
+my_variable = 4 #This is a declaration - Create
+my_varible = 5 #Assignment - modify
+favorite_animal = "Pygmy shark"
+print("Kaden's favorite animal is " + favorite_animal)
+# Concatination describes adding two strings together, 1+1 = 11 cat+dog= 
+# catdog, etc
+num = input("Enter a number:\n>")
+num = int(num)
+print(num * 10)
