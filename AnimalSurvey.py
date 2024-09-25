7cc8922876868e35fcc669349944d4b5c5567922
Ksteala5 <bartunekkaden@gmail.com>
2024-09-25 15:04:48 -0500

    ANIMAL SURVEY but Commented :)

--- AnimalSurvey.py
+++ AnimalSurvey.py
@@ -1,14 +1,14 @@
-favorite_animal = input("What is your favorite animal?\n>")
-print("So your favorite animal is a " + favorite_animal + ", cool!")
-animal_habitat = input("Where does this '" + favorite_animal + "' live?\n>")
-print("Ah, '" + animal_habitat + "'. Sounds comfy!")
-input("What do they look like?\n>")
+favorite_animal = input("What is your favorite animal?\n>")                     # Base question to determine fav. animal
+print("So your favorite animal is a " + favorite_animal + ", cool!")            # Response to make it more interactive
+animal_habitat = input("Where does this '" + favorite_animal + "' live?\n>")    # Habitat for later summary
+print("Ah, '" + animal_habitat + "'. Sounds comfy!")                            # Response
+input("What do they look like?\n>")                                             # Interactive only
 print("Interesting...")
 input("And what do they do?\n>")
 print("Wow, that seems pretty cool actually.")
-why = input("Why ARE they your favorite animal?\n>")
-print("Ahhhh.")
-food = input("What should this " + favorite_animal + " eat?\n>")
-print("I don't care much for " + food + ", as I am a computer system. But to each their own.")
-print("So your favorite animal is the " + favorite_animal + " because " + why + ", they eat " + food + ", and they live at the '" + animal_habitat + "'.")
-print("Goodbye for now! I learned a lot about your favorite animal, the " + favorite_animal + "! I think it is quite interesting as well!")
+why = input("Why ARE they your favorite animal?\n>")                            # For summary
+print("Ahhhh.")                                                                 # Response
+food = input("What should this " + favorite_animal + " eat?\n>")                # For summary
+print("I don't care much for " + food + ", as I am a computer system. But to each their own.")      # Interactive
+print("So your favorite animal is the " + favorite_animal + " because " + why + ", they eat " + food + ", and they live at the '" + animal_habitat + "'.")     # Summary
+print("Goodbye for now! I learned a lot about your favorite animal, the " + favorite_animal + "! I think it is quite interesting as well!") # Interactive goodbye
