ce25093c7a83602a53432bcb42213e1cd27ca3b4
Ksteala5 <bartunekkaden@gmail.com>
2024-09-26 15:12:41 -0500

    MADMANLibs

--- Unit 2 - Functions and Variables/SimpleMadLib.py
+++
@@ -1,4 +0,0 @@
-#The (adjective) (noun) felt (emotion)
-#as the (adjective) (noun) quickly approached. 
-#The (adjective) (noun) was too (adjective) to stop the (noun),
-#causing the (noun) to (adjective)
