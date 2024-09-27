d92801a2924a478629890d100af963ce71a9c79b
Ksteala5 <bartunekkaden@gmail.com>
2024-09-27 15:19:45 -0500

    FREGHBVJSAHGJ

---
+++ Unit 2 - Functions and Variables/functions.py
@@ -0,0 +1,8 @@
+def top5mvs(a,b,c,d,e): #Define the function wth a name
+    print("These are Kaden's top 5 movies:")
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(e)
+top5mvs("est","test","test","tes","tset")
