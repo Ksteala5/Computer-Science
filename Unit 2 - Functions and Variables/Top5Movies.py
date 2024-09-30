175f378644b1a7fae5a5354d32106429fa5b7bff
Ksteala5 <bartunekkaden@gmail.com>
2024-09-30 14:23:47 -0500

    Top 5 Movies

--- Unit 2 - Functions and Variables/Top5Movies.py
+++ Unit 2 - Functions and Variables/Top5Movies.py
@@ -5,4 +5,4 @@ def top5mvs(a,b,c,d,e): #Define the function wth a name
     print(c)
     print(d)
     print(e)
-top5mvs("est","test","test","tes","tset")
+top5mvs("HTTYD","HTTYD2","HTTYD3","Cars 2","Avatar")
