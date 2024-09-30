59a297913b76ffef5f13360b4545bbf807cd2b47
Ksteala5 <bartunekkaden@gmail.com>
2024-09-30 15:12:40 -0500

    sdfaguyfsva

--- Assignments/Simple_Calcuator.py
+++ Assignments/Simple_Calcuator.py
@@ -1,11 +1,20 @@
 def add(x,y):
     print(x+y)
+    return x + y
+add(1,3)
 
 def subtract(x,y):
     print(x-y)
+    return x - y
+subtract(1,3)
 
 def multiply(x,y):
     print(x * y)
+    return x * y
+multiply(1,3)
+
 
 def divide(x,y):
     print(x/y)
+    return x / y
+divide(1,3)
