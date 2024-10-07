e50094e2c3787592a90051dec579e7c6d13bd716
Ksteala5 <bartunekkaden@gmail.com>
2024-10-07 15:20:11 -0500

    vfgasv

---
+++ Unit 3 - Conditionals and Exceptions/booleanexpressions.py
@@ -0,0 +1,32 @@
+# Boolean Expressions
+# Kinda like a mathematical formula
+# 14 + 10 = 24      Algebra uses arithmetic operators
+# + - / * ** % //
+# Can only evaluate to True or False
+# 5 > 10 = False    Boolean uses comparison operators
+# > < >= <= == !=
+
+print(14+10)    # Algebra
+print(5>10)     # Boolean algebra
+
+x=5             # SET x = to 5
+print(5==5)     # == asks Are you equal?
+
+# This is the perfect test Q
+# What is the difference between = and ==? "=" makes things equal "==" asks
+print(4>=2)         # True
+print(1993==1993)   # True
+print(-90<-99)      # False
+print(10!=10)       # False , "not"
+
+
+# Boolean expressions quiz!
+answer_one = input ("Give me and ineger that is negative\n>")
+answer_one = int(answer_one)
+print(answer_one<0)
+
+answer_two = input("Write thee number 5\n>")
+print(answer_two=="5")
+
+print("Walrus"+"Walrus")    #WalrusWalrus
+print("Walrus"=="Walrus")   #True
---
+++ Unit 3 - Conditionals and Exceptions/ifstatements.py
@@ -0,0 +1,43 @@
+# If statements evaluate boolean expressions!
+# They make decisions about which code to run next
+#
+# Take a temperature
+# print "<temp> is hot" if >= to 80
+# other, print "<temp> is not hot"
+temp=input("So, you think it's hot? Whats the tempurature in F?\n>")
+temp=int(temp)
+
+#if <boolean expression>:
+#This should remind of writing a function
+if temp>= 80:
+    print(str(temp)+ "°F is hot!")
+
+else:     #Otherwise..
+    print(str(temp)+"°F isn't hot...")
+
+# Not all if statements need an else, none actually NEED an else, optional.
+# All else statements MUST have a corrosponding if statement
+# ELSE CANNOT EXIST alone.
+# An if can only have ONE else
+    
+
+
+    # Create a program that asks for a password
+
+# if password is correct, print ACCESS GRANTED
+# password is skibidi68
+permpass="skibidi68"
+password=input("INSERT PASSWORD:\n>")
+
+if password == permpass:
+    print("ACCESS GRANTED. WELCOME, MY KITTEN SIGMA ALPHA WOLF")
+else:
+    print("ACCESS DENIED YOU GOOBER")
+
+# Activity 2, electric boogaloo
+
+q1=input("say 'one'\n>")
+q2=input("say '6842'")
+q3=
+q4=
+q5=
