ddd49bdb535f007b2a76fd4ce281860138c0d6dd
Ksteala5 <bartunekkaden@gmail.com>
2024-10-08 15:04:04 -0500

    yur

--- Unit 3 - Conditionals and Exceptions/ifstatements.py
+++ Unit 3 - Conditionals and Exceptions/ifstatements.py
@@ -35,9 +35,36 @@ else:
     print("ACCESS DENIED YOU GOOBER")
 
 # Activity 2, electric boogaloo
+tally_score = 0
+q1=int(input("input '7'\n>"))
+q2=int(input("input '6842'\n>"))
+q3=int(input("input a WHOLE number LESS than 0\n>"))
+q4=int(input("input a WHOLE number GREATER than 0\n>"))
+q5=int(input("what is 2+2-2/2?\n>"))
 
-q1=input("say 'one'\n>")
-q2=input("say '6842'")
-q3=
-q4=
-q5=
+if q1 == 7:
+    tally_score = tally_score +1
+else:
+    tally_score = tally_score +0
+
+if q2 == 6842:
+    tally_score = tally_score +1
+else:
+    tally_score = tally_score +0
+if q3 < 0:
+    tally_score = tally_score +1
+else:
+    tally_score = tally_score +0
+
+if q4 > 0:
+    tally_score = tally_score +1
+else: 
+    tally_score = tally_score +0
+
+if q5 == 3:
+    tally_score = tally_score +1
+else: 
+    tally_score = tally_score +0
+
+tally_score = str(tally_score)
+print("Your score is " + tally_score + ".")
