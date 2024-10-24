292ad79e11961269507d9615348509ec1ff1f4c7
Ksteala5 <bartunekkaden@gmail.com>
2024-10-24 15:20:40 -0500

    UPDATE

--- Assignments/TextAdventureProject.py
+++ Assignments/TextAdventureProject.py
@@ -1,6 +1,7 @@
 name = "Input Name"
 gender = "Input Gender"
 interest = "Interest"
+weapon = "Weapon"
 def start():
     global name
     global gender
@@ -11,45 +12,65 @@ def start():
     print("Welcome, "+name+"!")
     
     gender = str(input("Next, choose your gender. Please choose M, F, or O (Other):\n>")).lower()
-    interest = str(input("Romantic interest? Please type 'B'(boyfriend), 'G'(girlfriend), or 'P'(partner).\n> ")).lower()
-    if interest == "b":
-         interest = "boyfriend"
-    elif interest == "g":
-         interest = "girlfriend"
-    elif interest == "p":
-         interest = "partner"
+    if gender == "m" or "f" or "o":
+        interest = str(input("Romantic interest? Please type 'B'(boyfriend), 'G'(girlfriend), or 'P'(partner).\n> ")).lower()
+        if interest == "b":
+             interest = "boyfriend"
+        elif interest == "g":
+             interest = "girlfriend"
+        elif interest == "p":
+             interest = "partner"
+        else:
+             print("Choose only b, g, or f.\n\n\n")
+             start()
+        if interest == "boyfriend" or "girlfriend" or "partner":
+            print("\n\n\nPerfect! Let's start! If you have options, answer with the corresponding number. Do not type out 'one', for example. Type '1' instead.\n\n\n")
+        else:
+             print("Please retry. Something went wrong. Sorry!")
+             start()
     else:
-         print("Choose only b, g, or f.\n\n\n")
-         start()
-    if interest == "boyfriend" or "girlfriend" or "partner":
-        print("\n\n\nPerfect! Let's start! If you have options, answer with the corresponding number. Do not type out 'one', for example. Type '1' instead.\n\n\n")
-    else:
-         print("Please retry. Something went wrong. Sorry!")
-         start()
+         print("Choose ONLY M, F, or O.")
 start()
 
-def step1():
+     
+def go():
         try:
             choice1 = int(input("You are sitting in your room, scrolling through Instagram reels on your phone.'" + name + "!' You"+
                                 " hear your "+interest+" call your name. Do you: \n 1. Do nothing. \n 2. Call back. \n 3. Go downstairs.\n\n>"))
             if choice1 == 1:
-                print("The reel you are watching must be important. You hear a crash downstairs after a minute, followed by a scream.")
-                choice2 = int(input("Do you\n 1. Investigate \n 2. Watch another reel \n 3."))                              #ADD CHOICESSSSS!!!!!!!!
+                print("The reel you are watching must be important. You hear a crash downstairs after a minute, followed by a scream.\n")
+                choice2 = int(input("\nDo you\n 1. Investigate \n 2. Watch another reel \n 3. Look for a weapon.\n\n>"))
+               
+                if choice2 == 1:
+                     downstairsbad()
+             
+                elif choice2 == 2:
+                     print("You watch another reel. As you are watching, a notification pops up. It reads: 'Warning: This is a public service announcement. Creatures"+
+                           " have been spotted around your area. They are of unknown origin and seem to be extremely hostile. Stay indoors, may God have mercy on us all.'"+
+                           " It lists many areas where the creatures have appeared. You decide to grab a weapon and head downstairs.\n\n")
+                     weaponchoice()
 
+                elif choice2 == 3:
+                     weaponchoice()
+                
+                else:
+                     print("Please choose 1 or 2.")
+                     
             elif choice1 == 2:
                 choice2 = int(input("'Yeah?' You call back. Your "+interest+" calls back a moment later. They sound nervous, which isn't like them."+
                                     " 'I saw something outside! Can you come down?'\n 1. Yes\n 2. No\n>")).lower()
                 if choice2 == 1:
-                     print("'Yeah, I'll be right down!' You respond.")                                                         #FINISH THIS
+                     print("'Yeah, I'll be right down!' You respond. You pocket your phone and make your way downstairs.")
+                     downstairsgood()                                                        
 
             elif choice1 == 3:
                 print("You attempt to pocket your phone and go downstairs. They sounded urgent. "+
                       "Unfortunately, you fall down the stairs while still fumbling with your phone, breaking your neck. Tough luck, retry?")
                 redo = str(input("\nRetry? \nY \nN \n\n> ")).upper()
-                if redo == "Y":
+                if redo == "Y" or "YES":
                      print("Yay!")
-                     step1()
-                elif redo == "N":
+                     go()
+                elif redo == "N" or "NO":
                      print("Lame, whatever. Bye.")
                 else:
                      print("Not sure what that means... RESTART IT IS! HAHAHAHAHAHAAA")
@@ -57,10 +78,51 @@ def step1():
 
             else:
                  print("Please try again, something went wrong.")
-                 step1()
+                 go()
 
         except ValueError: 
              print("\n\nNot funny. Next time, input a number like this: 1  OR  2. We already explained that."+
                    " Restart the story and read the rules this time.\n")
              start()
-step1()
+go()
+
+def downstairsgood():
+     print("\nUpon arriving downstairs, you see your "+interest+" standing a distance from the window, and staring out. They seem frightened.")
+
+
+def downstairsbad():
+     print("\nYou make your way downstairs, phone in hand. A heavy weight is in the air, feeling like a thick slurry. The house is unnaturally dark. Audible heavy panting is heard.\n\n")
+     light = input("Turn on phone flashlight? \nY \nN").upper()
+     if light == "Y":
+          print("")
+
+def roomunarmed():
+     print()
+
+def weaponchoice():
+    global weapon
+    try: 
+         weapon = str(input("You have a few items that could be used as a weapon in your room. \n 1. Wooden Bat \n 2. Metal Trophy \n 3. Metal Stool\n\n"))
+         if weapon==1:
+              weapon = "bat"
+              print("You pick up the "+weapon+" and head downstairs.")
+              downstairsbat()
+
+         elif weapon==2:
+              weapon = "trophy"
+              print("You pick up the "+weapon+" and head downstairs.")
+              downstairstrophy()
+
+         elif weapon==3:
+              weapon = "stool"
+              print("You pick up the "+weapon+" and head downstairs.")
+              downstairsstool()
+
+         else:
+            print("\n Please choose 1, 2, or 3.")
+    
+    
+    except ValueError:
+         print("\n\nNot funny. Next time, input a number like this: 1  OR  2. We already explained that."+
+                   " Restart the story and read the rules this time.\n")
+         start()
