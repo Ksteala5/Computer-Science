df7fc92455beeb3a886ba35ec2774046b25fd9c7
Ksteala5 <bartunekkaden@gmail.com>
2024-11-04 15:14:02 -0600

    what a save

--- Assignments/manualsorting.py
+++ Assignments/manualsorting.py
@@ -1,14 +1,14 @@
 import random
-a = random.randint(1,101)
-b = random.randint(1,101)
-c = random.randint(1,101)
-d = random.randint(1,101)
-e = random.randint(1,101)
-numbers = [a,b,c,d,e]
+numbers = [random.randint(1,101),random.randint(1,101),random.randint(1,101),random.randint(1,101),random.randint(1,101),]
+print(numbers)
 def bubble_sort(n):
-    for i in range(0, len(n)-1):
-        if n[i]>n[i+1]:
-            n[i], n[i+1] = n[i+1], n[i]
+    steps = 0
+    for j in range (0, len(n)-1):
+        for i in range(0, len(n)-1):
+            if n[i]>n[i+1]:
+                n[i], n[i+1] = n[i+1], n[i]
+                steps += 1
     print(n)
+    print(str(steps)+" steps.")
 
 bubble_sort(numbers)
